9a914baa6424c9ee2e454da85c2858258dfde59d
David Shunfenthal <dshunfen@users.noreply.github.com>
2022-07-07 09:27:17 -0400

    Ensure Brew tap URL matches release path
    This had errored for me when doing an upgrade

--- Formula/baselime.rb
+++ Formula/baselime.rb
@@ -1,7 +1,7 @@
 class Baselime < Formula
   desc "Observability as code for serverless"
   homepage "https://github.com/baselime/cli"
-  url "https://github.com/baselime/cli/releases/download/v0.0.5/baselime-darwin-x64-v0.0.6.tar.gz"
+  url "https://github.com/baselime/cli/releases/download/v0.0.6/baselime-darwin-x64-v0.0.6.tar.gz"
   sha256 "0e8b3c79f38d2156c0f732e95a02e3ca5dbb981527bedf183a135ff2f342f0c6"
   license "MIT"
 
