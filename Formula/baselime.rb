e98fc28ba503fa37ebb555ceec8da28b112950ee
Boris Tane <boris.tane@gmail.com>
2022-10-08 17:48:43 +0100

    v0.0.20

--- Formula/baselime.rb
+++ Formula/baselime.rb
@@ -1,8 +1,8 @@
 class Baselime < Formula
   desc "Observability as Code for serverless"
   homepage "https://github.com/baselime/cli"
-  url "https://github.com/baselime/cli/releases/download/v0.0.19/baselime-darwin-x64-v0.0.19.tar.gz"
-  sha256 "b59550692745df04d4bc2b9419a4a082abfaa22f0a9bc9643a8107dbcb25665d"
+  url "https://github.com/baselime/cli/releases/download/v0.0.20/baselime-darwin-x64-v0.0.20.tar.gz"
+  sha256 "11adb4f656113d681c3b406e86702748598b9e061ba3b28ccccea099fda10c0e"
   license "MIT"
 
   def install
