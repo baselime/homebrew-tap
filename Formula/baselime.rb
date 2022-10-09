91569f173e4687a9a97ac7139606e65bb7fec841
Boris Tane <boris.tane@gmail.com>
2022-10-09 16:53:52 +0100

    add version to formula

--- Formula/baselime.rb
+++ Formula/baselime.rb
@@ -3,6 +3,7 @@ class Baselime < Formula
   homepage "https://github.com/baselime/cli"
   url "https://github.com/baselime/cli/releases/download/v0.0.20/baselime-darwin-x64-v0.0.20.tar.gz"
   sha256 "11adb4f656113d681c3b406e86702748598b9e061ba3b28ccccea099fda10c0e"
+  version "0.0.20"
   license "MIT"
 
   def install
